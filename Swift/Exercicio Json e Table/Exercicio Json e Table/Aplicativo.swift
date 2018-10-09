60f765674af3b860387dfd34a09601232691da20
Usuário Convidado <Guest@L2602MICRO17.local>
2018-10-08 22:26:02 -0300

    Exemplo Consumo json

---
+++ Swift/Exercicio Json e Table/Exercicio Json e Table/Aplicativo.swift
@@ -0,0 +1,16 @@
+//
+//  Aplicativo.swift
+//  Exercicio Json e Table
+//
+//  Created by Usuário Convidado on 08/10/2018.
+//  Copyright © 2018 Joao Victor. All rights reserved.
+//
+
+import UIKit
+
+class Aplicativo: NSObject {
+    var nome:String=""
+    var rua:String=""
+    var cidade:String=""
+    var genero:String=""
+}
